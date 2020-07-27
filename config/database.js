d35c1c3e09eef9626c45804158ee6c3862420d1f
Christin Mikolai <mail@christinmikolai.de>
2020-07-27 23:38:30 +0200

    mongodb change uri test

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         client: 'mongo',
-        uri: "mongodb://admin-christin:Gf6-G99-Ffa@christinmikolai-portfol-shard-00-00.yigez.mongodb.net:27017,christinmikolai-portfol-shard-00-01.yigez.mongodb.net:27017,christinmikolai-portfol-shard-00-02.yigez.mongodb.net:27017/christinmikolai-portfolio?ssl=true&replicaSet=atlas-t9gg44-shard-0&authSource=admin&retryWrites=true&w=majority",
+        uri: "${process.env.MONGODB_URI}",
       },
       options: {
         ssl: true
