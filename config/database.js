fffc40cea99c879a8e3b9efa2bfb1cb5f194f5ed
Christin Mikolai <mail@christinmikolai.de>
2020-07-27 23:46:21 +0200

    testing out another string

---
+++ config/database.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        client: 'mongo',
+        uri: `${process.env.MONGODB_URI}`,
+      },
+      options: {
+        ssl: true
+      }
+    },
+  },
+});
